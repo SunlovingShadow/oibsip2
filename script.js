33395fc8cd4f2de05570805dd43cd8d676616430
Samadrita Mukherjee <97374137+SunlovingShadow@users.noreply.github.com>
2024-06-03 23:11:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function playVideo() {
+    var videoPlaceholder = document.getElementById('video-placeholder');
+    videoPlaceholder.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tuw8hxrFBH8?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+}
